souvik0306/PX4-Autopilot

Add Altitude Cruise mode
-add new NAVIGATION_STATE_ALTITUDE_VOYAGER
-this mode does require manual control to enter
-but you can disable the manual control loss failsafe to continue
flying in case of manual control loss
-for MC: in throttle and yaw are controlled like in Altitude mode,
the tilt is controlled via integrated rate input (similar to Acro,
but with tilt limit)

Signed-off-by: Silvan Fuhrer <silvan@auterion.com>

src/modules/commander/ModeUtil/conversions.hpp
@@ -50,6 +50,8 @@ static inline navigation_mode_t navigation_mode(uint8_t nav_state)
 
 	case vehicle_status_s::NAVIGATION_STATE_ALTCTL: return navigation_mode_t::altctl;
 
+	case vehicle_status_s::NAVIGATION_STATE_ALTITUDE_CRUISE: return navigation_mode_t::manual_altitude_cruise;
+
 	case vehicle_status_s::NAVIGATION_STATE_POSCTL: return navigation_mode_t::posctl;
 
 	case vehicle_status_s::NAVIGATION_STATE_AUTO_MISSION: return navigation_mode_t::auto_mission;

src/modules/flight_mode_manager/tasks/AltitudeCruise/FlightTaskAltitudeCruise.cpp
@@ -1,0 +1,19 @@
+#include "FlightTaskAltitudeCruise.hpp"
+
+FlightTaskAltitudeCruise::FlightTaskAltitudeCruise()
+{
+	_sticks_data_required = false; // disable stick requirement to not report flight task failure when they're lost
+}
+
+void FlightTaskAltitudeCruise::reActivate()
+{
+	FlightTaskManualAltitude::reActivate();
+	_stick_tilt_xy.reset();
+}
+
+void FlightTaskAltitudeCruise::_updateXYSetpoint()
+{
+	_acceleration_setpoint.xy() =
+		_stick_tilt_xy.generateAccelerationSetpointsForAltitudeCruise(
+			_sticks.getPitchRoll(), _deltatime, _yaw, _yaw_setpoint);
+}

src/modules/flight_mode_manager/tasks/AltitudeCruise/FlightTaskAltitudeCruise.hpp
@@ -1,0 +1,15 @@
+#pragma once
+
+#include "FlightTaskManualAltitude.hpp"
+
+class FlightTaskAltitudeCruise : public FlightTaskManualAltitude
+{
+public:
+	FlightTaskAltitudeCruise();
+	virtual ~FlightTaskAltitudeCruise() = default;
+
+	void reActivate() override;
+
+protected:
+	void _updateXYSetpoint() override;
+};

src/modules/flight_mode_manager/tasks/Utility/StickTiltXY.cpp
@@ -44,6 +44,11 @@ StickTiltXY::StickTiltXY(ModuleParams *parent) :
 	updateParams();
 }
 
+void StickTiltXY::reset()
+{
+	_altitude_cruise_acceleration.setZero();
+}
+
 void StickTiltXY::updateParams()
 {
 	ModuleParams::updateParams();
@@ -59,6 +64,26 @@ Vector2f StickTiltXY::generateAccelerationSetpoints(Vector2f stick_xy, const flo
 	Sticks::limitStickUnitLengthXY(stick_xy);
 	_man_input_filter.setParameters(dt, _param_mc_man_tilt_tau.get());
 	stick_xy = _man_input_filter.update(stick_xy);
+
 	Sticks::rotateIntoHeadingFrameXY(stick_xy, yaw, yaw_setpoint);
+
 	return stick_xy * _maximum_acceleration;
+}
+
+Vector2f StickTiltXY::generateAccelerationSetpointsForAltitudeCruise(Vector2f stick_xy, const float dt, const float yaw,
+		const float yaw_setpoint)
+{
+	Sticks::limitStickUnitLengthXY(stick_xy);
+	const Vector2f increment = stick_xy;
+	_altitude_cruise_acceleration += increment * _maximum_acceleration * 2.f *
+					 dt; // at full stick deflection it takes 1s from -tilt_max to tilt_max
+
+	if (_altitude_cruise_acceleration.longerThan(_maximum_acceleration)) {
+		_altitude_cruise_acceleration =
+			_altitude_cruise_acceleration.unit_or_zero() * _maximum_acceleration;
+	}
+
+	auto global_altitude_cruise_acceleration = _altitude_cruise_acceleration;
+	Sticks::rotateIntoHeadingFrameXY(global_altitude_cruise_acceleration, yaw, yaw_setpoint);
+	return global_altitude_cruise_acceleration;
 }

AshhadRasheed/PX4-Autopilot-experimental

src/modules/commander/ModeUtil/conversions.hpp
@@ -50,6 +50,8 @@ static inline navigation_mode_t navigation_mode(uint8_t nav_state)
 
 	case vehicle_status_s::NAVIGATION_STATE_ALTCTL: return navigation_mode_t::altctl;
 
+	case vehicle_status_s::NAVIGATION_STATE_ALTITUDE_CRUISE: return navigation_mode_t::altitude_cruise;
+
 	case vehicle_status_s::NAVIGATION_STATE_POSCTL: return navigation_mode_t::posctl;
 
 	case vehicle_status_s::NAVIGATION_STATE_AUTO_MISSION: return navigation_mode_t::auto_mission;

src/modules/flight_mode_manager/tasks/AltitudeCruise/FlightTaskAltitudeCruise.cpp
@@ -1,0 +1,19 @@
+#include "FlightTaskAltitudeCruise.hpp"
+
+FlightTaskAltitudeCruise::FlightTaskAltitudeCruise()
+{
+	_sticks_data_required = false; // disable stick requirement to not report flight task failure when they're lost
+}
+
+void FlightTaskAltitudeCruise::reActivate()
+{
+	FlightTaskManualAltitudeSmoothVel::reActivate();
+	_stick_tilt_xy.reset();
+}
+
+void FlightTaskAltitudeCruise::_updateXYSetpoint()
+{
+	_acceleration_setpoint.xy() =
+		_stick_tilt_xy.generateAccelerationSetpointsForAltitudeCruise(
+			_sticks.getPitchRoll(), _deltatime, _yaw, _yaw_setpoint);
+}

src/modules/flight_mode_manager/tasks/AltitudeCruise/FlightTaskAltitudeCruise.hpp
@@ -1,0 +1,15 @@
+#pragma once
+
+#include "FlightTaskManualAltitudeSmoothVel.hpp"
+
+class FlightTaskAltitudeCruise : public FlightTaskManualAltitudeSmoothVel
+{
+public:
+	FlightTaskAltitudeCruise();
+	virtual ~FlightTaskAltitudeCruise() = default;
+
+	void reActivate() override;
+
+protected:
+	void _updateXYSetpoint() override;
+};

src/modules/flight_mode_manager/tasks/Utility/StickTiltXY.cpp
@@ -44,6 +44,11 @@ StickTiltXY::StickTiltXY(ModuleParams *parent) :
 	updateParams();
 }
 
+void StickTiltXY::reset()
+{
+	_altitude_cruise_acceleration.setZero();
+}
+
 void StickTiltXY::updateParams()
 {
 	ModuleParams::updateParams();
@@ -61,4 +66,22 @@ Vector2f StickTiltXY::generateAccelerationSetpoints(Vector2f stick_xy, const flo
 	stick_xy = _man_input_filter.update(stick_xy);
 	Sticks::rotateIntoHeadingFrameXY(stick_xy, yaw, yaw_setpoint);
 	return stick_xy * _maximum_acceleration;
+}
+
+Vector2f StickTiltXY::generateAccelerationSetpointsForAltitudeCruise(Vector2f stick_xy, const float dt, const float yaw,
+		const float yaw_setpoint)
+{
+	Sticks::limitStickUnitLengthXY(stick_xy);
+	const Vector2f increment = stick_xy;
+	// at full stick deflection it takes 1s from -tilt_max to tilt_max
+	_altitude_cruise_acceleration += increment * _maximum_acceleration * 2.f * dt;
+
+	if (_altitude_cruise_acceleration.longerThan(_maximum_acceleration)) {
+		_altitude_cruise_acceleration =
+			_altitude_cruise_acceleration.unit_or_zero() * _maximum_acceleration;
+	}
+
+	Vector2f global_altitude_cruise_acceleration = _altitude_cruise_acceleration;
+	Sticks::rotateIntoHeadingFrameXY(global_altitude_cruise_acceleration, yaw, yaw_setpoint);
+	return global_altitude_cruise_acceleration;
 }